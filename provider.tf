0f67c467db7fda61650dbe8d415d56399621ae84
Manoj Kumar T <43432156+M4N0J-KUM4R@users.noreply.github.com>
2025-04-06 11:46:42 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.90.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "default"
+  alias = "mumbai"
+}
+
+provider "aws" {
+  region = "ap-south-2"
+  profile = "ram"
+  alias = "hydrabad"
+}
